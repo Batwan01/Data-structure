a6562b48857e58d32e6edaa02465fb461d63346b
Batwan01 <wldhks1120@naver.com>
2023-04-13 23:24:39 +0900

    P135-17

--- Data structure/P135-17.c
+++ Data structure/P135-17.c
@@ -1,18 +1,84 @@
 #include <stdio.h>
-#include <memory.h>
 #include <stdlib.h> //malloc함수를 사용하기 위한 헤더 파일
+#include <string.h>
 
-typedef struct test {
-	int data;
-	struct test* link;
-} test_str;
+typedef struct Node { //노드 생성
+	char *name;
+	struct Node* next; //이중 연결리스트 생성
+	struct Node* prev;
+} Node;
 
+typedef struct List { //head와 tail 정의
+	Node* head;
+	Node* tail;
+	Node* first;
+	int count;
+}List;
+
+Node* Newnode(char *names) //노드 새로 만들기
+{
+	Node* new = (Node*)malloc(sizeof(Node)); //구조체 1개 공간 할당
+	new->name = (char*)malloc(strlen(names) + 1);
+	strcpy(new->name, names);
+	new->next = new->prev = NULL;
+	return new;	
+}
+
+void Setnode(List* list) { //노드 초기설정
+	list->head = Newnode(" ");
+	list->tail = Newnode(" ");
+	list->head->next = list->tail; //head의 다음은 tail
+	list->head->prev = list->tail; //head의 뒤는 tail
+	list->tail->next = list->head; //tail의 다음은 head
+	list->tail->prev = list->head; //tail의 뒤는 head
+	list->count = 0;
+}
+
+void Adddata(List* list, char* names) {
+	Node *add = Newnode(names);
+	if (list->count == 0) { //첫번째 tail 설정
+		add->next = list->head->next;
+		add->prev = list->head;
+		list->head->next = add;
+		list->tail->prev = add;
+	}
+	else {
+		add->next = list->head->next;
+		add->prev = list->head;
+		list->head->next = add;
+	}
+	list->count++;
+}
+void Freelist(List* list) { //head와 tail값 free하기
+	Node* heads = list->head;
+	Node* tails = list->tail;
+	list->head->next->prev = list->tail->prev; //head다음 첫 번쨰 값의 prev는 tail의 prev값
+	list->tail->prev->next = list->tail->next;//tail뒤의 값의 next값은 tail의 next값
+	Node *first = list->head->next; //첫번째 노드 first
+}
+
+void Viewall(List* list) {
+	Node* check = list->first;
+	int i = 0;
+	while (i!=15)
+	{
+		printf("%s",check->name);
+		check = check->next;
+		i++;
+	}
+}
 void main()
 {
 	int i;
-	test_str* ptest;
-	ptest = (double*)malloc(sizeof(test_str));
-	ptest->data = 100;
-	printf("%d", ptest->data);
-	free(ptest);
-}
+	char names[10];
+	List list;
+	Setnode(&list);
+	printf("학생의 이름을 입력하세요.(종료를 원하면 0을 입력하세요.)\n");
+	while (1) {
+ 		scanf("%s", names);
+		if (names[0] == '0') break;
+		Adddata(&list, names);
+	}
+	Freelist(&list);
+	Viewall(&list);
+}	
---
+++ Data structure/solution.c
@@ -0,0 +1,143 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {   // 구조체 이중 연결리스트에 사용될 Node
+    struct Node* next;
+    struct Node* prev;
+    char name[10];
+}Node;
+
+Node* creat_node() { // 노드 생성 함수
+    Node* NEW = (Node*)malloc(sizeof(Node));
+    NEW->next = NULL;
+    NEW->prev = NULL;
+    return NEW;
+}
+
+void make_circular_doubly_linked_list(Node* head, Node* tail) {   // 원형 이중 연결 리스트 생성
+    head->next = tail;
+    tail->next = head;
+    head->prev = tail;
+    tail->prev = head;
+}
+
+/*원형 연결 리스트에 노드 추가(마지막에 추가된 노드 제외)*/
+Node* Add_Node(Node* Before, Node* tail, char* Student_name) {
+    Node* NEW = (Node*)malloc(sizeof(Node));
+    NEW->prev = Before;
+    Before->next = NEW;
+    strcpy(NEW->name, Student_name);
+    return NEW;
+}
+
+/* head노드와 tail노드를 제외시킨 원형 이중 연결 리스트를 만들고
+처음으로 입력된 이름이 저장된 노드를 가리키는 포인터 First 반환 */
+Node* head_tail_Remove_ReturnFirst(Node* head, Node* tail) {
+    head->next->prev = tail->prev;
+    tail->prev->next = head->next;
+    Node* First = head->next;
+    return First;
+}
+
+/*난수를 이용해 뽑힐 사람 결정
+먼저 입력된 사람부터 1, 2, 3 ... 순으로 증가 */
+Node* Random_Choice_Student(Node* First, int Number_Of_Student) {
+    srand((int)time(NULL)); // 난수 생성
+    // 먼저 입력된 이름 부터 그 다음 이름 순으로 1, 2, 3, ... 별칭 지정한다고 가정
+    int choice = ((rand() % Number_Of_Student) + 1); // 뽑힐 사람 결정
+    Node* target = First;
+    for (int i = 1; i < choice; i++) {
+        target = target->next;
+    }
+    printf("뽑힌 학생 : %s\n", target->name);
+    return target;
+}
+
+/* 노드 삭제 함수 */
+void Remove_Node(Node* target) {
+    target->next->prev = target->prev;
+    target->prev->next = target->next;
+    free(target);
+}
+/* 시계 방향으로 N번째 사람 제거(숫자 셀 때 자신은 미 포함)
+   남은 사람 출력*/
+Node* Clockwise_Remove_Linked_List(Node* target, int N, int Number_Of_Student) {
+    for (int i = 0; i < N; i++) {
+        target = target->next;
+    }
+    printf("제거 될 사람 %s\n", target->name);
+    Node* Next_target = target->next;
+    Remove_Node(target);
+    for (int i = 0; i < Number_Of_Student - 1; i++) {
+        printf("%s- ", Next_target->name);
+        Next_target = Next_target->next;
+    }
+    printf("\n");
+    return Next_target;
+}
+
+/* 반시계 방향으로 N번째 사람 제거(숫자 셀 때 자신은 미 포함)
+   남은 사람 출력*/
+Node* CounterClockwise_Remove_Linked_List(Node* target, int N, int Number_Of_Student) {
+    for (int i = 0; i < N; i++) {
+        target = target->prev;
+    }
+    printf("제거 될 사람 %s\n", target->name);
+    Node* Next_target = target->prev;
+    Remove_Node(target);
+    for (int i = 0; i < Number_Of_Student - 1; i++) {
+        printf("%s- ", Next_target->name);
+        Next_target = Next_target->next;
+    }
+    printf("\n");
+    return Next_target;
+}
+int main(void) {
+    Node* head = creat_node();
+    Node* tail = creat_node();
+    make_circular_doubly_linked_list(head, tail);
+    printf("참가할 학생 수 입력 : ");
+    int Number_Of_Student = 0;                  // 학생 수
+    int fit1 = scanf("%d", &Number_Of_Student);      // 입력이 올바르지 않으면 0이하의 값을 반환
+    if (fit1 <= 0) {
+        printf("올바른 값이 아닙니다.");
+        return -1;                            // 입력이 올바르지 않으면 종료
+    }
+    Node* Before = head;
+    for (int i = 0; i < Number_Of_Student; i++) {   // 학생 수 만큼 반복
+        printf("이름을 입력하세요 : ");
+        char Student_Name[10];
+        scanf("%s", Student_Name);
+        Before = Add_Node(Before, tail, Student_Name);
+    }
+    /*마지막에 생성된 노드도 연결해주기*/
+    Before->next = tail;
+    tail->prev = Before;
+    /* head와 tail을 제외한 나머지 노드들로 연결리스트를 만들어주고
+    head노드와 tail노드 메모리 반환*/
+    Node* First = head_tail_Remove_ReturnFirst(head, tail);
+    free(head);
+    free(tail);
+    int N;
+    printf("숫자 하나(N) 입력: ");
+    int fit2 = scanf("%d", &N);      // 입력이 올바르지 않으면 0이하의 값을 반환
+    if (fit2 <= 0) {
+        printf("올바른 값이 아닙니다.");
+        return -1; // 입력이 올바르지 않으면 종료
+    }
+    /* 난수로 선정된 숫자를 기준으로 이름을 뽑힌 사람을 정함 */
+    Node* target = Random_Choice_Student(First, Number_Of_Student);
+    while (Number_Of_Student != 1) { // 한 사람만 남으면 종료
+        target = Clockwise_Remove_Linked_List(target, N, Number_Of_Student); // 시계방향으로 N번째 제거
+        Number_Of_Student--;
+        if (Number_Of_Student == 1) // 한 사람만 남으면 종료
+            break;
+        target = CounterClockwise_Remove_Linked_List(target, N, Number_Of_Student); // 반시계방향으로 N번째 제거
+        Number_Of_Student--;
+    }
+    printf("마지막으로 남은 사람 %s", target->name);
+    free(target);
+    return 0;
+}
