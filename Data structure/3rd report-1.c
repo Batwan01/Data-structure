860a56bc0af3fbb0bf10cfdd0abb2fc769ed6692
Batwan01 <wldhks1120@naver.com>
2023-05-18 16:33:14 +0900

    1

--- Data structure/3rd report-1.c
+++ Data structure/3rd report-1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include<string.h>
 
 typedef struct ArrayStack {
-	int top;
-	int in_srt[100];
+	int top; //꼭대기
+	int in_str[100]; //배열의 크기 100
 } ArrayStack;
 
 int palindrome(char in_str[]) {
@@ -12,25 +14,30 @@ int palindrome(char in_str[]) {
 	char ch, chs;
 	int len = strlen(in_str);
 
-	        ; // 스택을 초기화하라
+	s.top = -1;//초기값 -1
 	for (i = 0; i < len; i++) {
-		ch = in_str[i];
+		ch = in_str[i]; 
 		// 만약 ch가 스페이스거나 구두점이면
-		if (         ) continue;
+		if (ch>31 && ch < 48) continue;
 		ch = tolower(ch); // ch를 소문자로 변경
-		          ; // 스택에 삽입한다.
+		s.in_str[++s.top] = ch; //top을 먼저 +1하고, ch값을 저장한다.
 	}
 	for (i = 0; i < len; i++) {
 		ch = in_str[i];
 		// 만약 ch가 스페이스거나 구두점이면
-		if (          ) continue;
+		if (ch > 31 && ch < 48) continue;
 		ch = tolower(ch); // ch를 소문자로 변경
-		chs =           ; // 스택에서 문자를 꺼낸다
-		if (ch != chs) return flase; // 실패
+		chs = s.in_str[s.top--]; // ch값을 chs에 저장하고, top을 -1한다.
+		if (ch != chs) return false; // 실패(0)
 	}
-	return true; // 성공
+	return true; // 성공(1)
 }
 
 int main(void) {
-
+	ArrayStack s;
+	int a;
+	scanf("%[^\n]s", s.in_str);
+	a = palindrome(s.in_str);
+	if (a == 1) printf("회문입니다.");
+	else printf("회문이 아닙니다.");
 }
