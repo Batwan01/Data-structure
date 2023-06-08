8b41dbfd15f64a9ccd243a318419c8efc9832754
Batwan01 <wldhks1120@naver.com>
2023-06-08 19:07:33 +0900

    sum

--- Data structure/pra.c
+++ Data structure/pra.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int recur(int n) {
+/*int recur(int n) {
 	printf("%d ", n);
 	if (n < 1) return -1;
 	else return (recur(n - 3) + 1);
-}
+}*/
  
+int sum(int n) {
+	if (n < 1) return 0;
+	return sum(n - 1) + 1/n;
+}
+
 int main(void)
 {
-	int n = 13;
-	recur(13);
+	printf("%d",sum(4));
 }
