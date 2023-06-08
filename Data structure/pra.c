28cf5a696d43e0cb742541044b5ee3d267aaba67
Batwan01 <wldhks1120@naver.com>
2023-06-08 17:16:39 +0900

    recur

---
+++ Data structure/pra.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int recur(int n) {
+	printf("%d ", n);
+	if (n < 1) return -1;
+	else return (recur(n - 3) + 1);
+}
+
+int main(void)
+{
+	int n = 13;
+	recur(13);
+}
